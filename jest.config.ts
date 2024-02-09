8293ec720731a55d0a57a18647fdb57021c22075
Alfredo Garcia <garciazambranoalfredo@gmail.com>
2024-02-08 21:50:41 -0400

    ADD installed ts-node, ts-jest and jest to write tests

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import type { JestConfigWithTsJest } from "ts-jest";
+
+const jestConfig: JestConfigWithTsJest = {
+  preset: "ts-jest/presets/default-esm",
+  extensionsToTreatAsEsm: [".ts"],
+  transform: {
+    "^.+\\.[tj]sx?$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
+  },
+};
+
+export default jestConfig;
